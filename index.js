b843e65fcf69b057521b68d0081c11fdc99da2e7
Adi Ahmad Danish <72914828+MagusWyvern@users.noreply.github.com>
2023-06-21 13:55:34 +0000

    Attempt #1 Script Precedence Fix

--- index.js
+++ index.js
@@ -35,47 +35,50 @@ auth.languageCode = 'en';
 // Additional scope requests can be passed as an array of strings.
 provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
 
-signInButton = document.getElementById('signInButton');
+document.onload = function() {
+  signInButton = document.getElementById('signInButton');
 
-signInButton.addEventListener('click', () => {
+  signInButton.addEventListener('click', () => {
+  
+    signInWithRedirect(auth, provider);
+  }  
+    )
+  
+  getRedirectResult(auth)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access Google APIs.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+  
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+  
+      console.log("Sign in successful! Signed in as: " + result.user.displayName)
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+  
+  // 
+  
+  signOutButton = document.getElementById('signOutButton');
+  
+  signOutButton.addEventListener('click', () => { 
+  
+    signOut(auth).then(() => {
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
+    });
+  
+  })
+}
 
-  signInWithRedirect(auth, provider);
-}  
-  )
-
-getRedirectResult(auth)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access Google APIs.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-
-    // The signed-in user info.
-    const user = result.user;
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
-
-    console.log("Sign in successful! Signed in as: " + result.user.displayName)
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
-
-// 
-
-signOutButton = document.getElementById('signOutButton');
-
-signOutButton.addEventListener('click', () => { 
-
-  signOut(auth).then(() => {
-    // Sign-out successful.
-  }).catch((error) => {
-    // An error happened.
-  });
-
-})
